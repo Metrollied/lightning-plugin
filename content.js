6fe7244aeddcfcd4e1dd4d328855e7cfb3b17e6d
Oliver Crawford <91338606+Metrollied@users.noreply.github.com>
2024-03-18 15:18:34 +0000

    he's slidin

--- content.js
+++ content.js
@@ -5,6 +5,7 @@ notification.className = 'jimmy-div';
 // Notification icon.
 const icon = document.createElement('img');
 icon.src = chrome.runtime.getURL("images/mad_skills.png");
+icon.className = "jimmy-img"
 notification.appendChild(icon);
 
 // Add to current page.
@@ -16,10 +17,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
     const notification = document.getElementsByClassName('jimmy-div')[0];
 
-    notification.style.display = 'flex';
+    notification.style.translate = '0';
 
     setTimeout(function () {
-        notification.style.display = 'none';
+        notification.removeAttribute("style");
     }, 3000);
     
     return true;
--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,8 @@
 chrome.commands.onCommand.addListener(async (command) => {
-    switch (command) {
-        case 'radical':
-            const tab = await getActiveTab();
-            await chrome.tabs.sendMessage(tab.id, {
-            tabTitle: tab.title
-    });
-
-    await PageService.savePage(tab.title, tab.url);
-            break;
-        default:
-            console.log(`Command ${command} not found`);
-    }
+    const tab = await getActiveTab();
+        await chrome.tabs.sendMessage(tab.id, {
+        tabTitle: tab.title
+    })
 });
 
 getActiveTab = async () => {
